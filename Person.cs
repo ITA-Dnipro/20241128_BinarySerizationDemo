cdf44f4cf21453dba1f2721080ab8699ce991165
apyura <prlxndr@ua.fm>
2024-11-28 20:04:56 +0200

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20241128_BinarySerizationDemo
+{
+    [Serializable]
+    public class Person : ICloneable
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Age { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} - {1} ({2})", Id, Name, Age);
+        }
+
+        public object Clone()
+        {
+            MemoryStream strm = new MemoryStream();
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+
+            binaryFormatter.Serialize(strm, this);
+
+            strm.Position = 0;
+
+            return binaryFormatter.Deserialize(strm);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace _20241128_BinarySerizationDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person() { Id = 0xFFFF, Name = "Dmytro", Age = 18.5 };
+
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+
+            using (FileStream fsOut = new FileStream("person.bin", FileMode.Create, FileAccess.Write))
+            {
+                binaryFormatter.Serialize(fsOut, p);
+            }
+
+            using (FileStream fsIn = new FileStream("person.bin", FileMode.Open, FileAccess.Read))
+            {
+                Person pCopy = (Person)binaryFormatter.Deserialize(fsIn);
+            }
+
+            Person pCopy2 = (Person)p.Clone();
+
+            Console.WriteLine("     p: {0}", p);
+            Console.WriteLine("pCopy2: {0}", pCopy2);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            pCopy2.Id = 223;
+
+            Console.WriteLine("     p: {0}", p);
+            Console.WriteLine("pCopy2: {0}", pCopy2);
+
+            Person[] people = new Person[] { p, pCopy2 };
+
+            using (FileStream fsOut = new FileStream("people.bin", FileMode.Create, FileAccess.Write))
+            {
+                binaryFormatter.Serialize(fsOut, people);
+            }
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("peopleCopy:");
+
+            using (FileStream fsIn = new FileStream("people.bin", FileMode.Open, FileAccess.Read))
+            {
+                Person[] peopleCopy = (Person[])binaryFormatter.Deserialize(fsIn);
+                for (int i = 0; i < peopleCopy.Length; i++)
+                {
+                    Console.WriteLine(peopleCopy[i]);
+                }
+            }
+
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
+        }
+    }
+}
